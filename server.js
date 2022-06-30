b0f184866913f3227ade25b7873a872b8c2140f4
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-30 12:23:08 -0500

    multer added

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const multer = require('multer')
+
 const express = require('express')
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+const upload = multer({ dest: 'uploads'})
 
 
 app.set('view engine', 'ejs')
@@ -10,6 +14,10 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('hi')
 })
 
 app.listen(PORT, ()=>{
